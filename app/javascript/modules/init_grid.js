45009afff6fa0b456f9d35824bfc0884077e52b2
Max Synnott <maxryansynnott@gmail.com>
2019-08-27 15:38:47 +0200

    added and removed some comments

--- app/javascript/modules/init_grid.js
+++ app/javascript/modules/init_grid.js
@@ -1,15 +1,13 @@
 const initGrid = () => {
   var canvas = document.getElementById('grid');
   if (canvas.getContext) {
-    // Selects draw context and pixel color
     var ctx = canvas.getContext('2d');
-    //
+    // Grabs data stored in the canvas tag
     const placements = JSON.parse(canvas.dataset.placements);
     const pixel_size = canvas.dataset.pixelSize
+    // Iterates over the placements, drawing each one to the grid
     placements.forEach((placement) => {
-      console.log(placement);
       ctx.fillStyle = placement['color'];
-      // console.log(`x: ${placement['x']}, y: ${placement['x']}, color: ${placement['color']}, pixel_size: ${pixel_size}`)
       ctx.fillRect(placement['x'], placement['y'], pixel_size, pixel_size);
     })
   } else {
