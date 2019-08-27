73b97c000f54cf4d051dd21a15647af1578eddd2
Max Synnott <maxryansynnott@gmail.com>
2019-08-28 00:50:29 +0200

    Converted javascript to accept and display using imageData method

--- app/javascript/modules/init_grid.js
+++ app/javascript/modules/init_grid.js
@@ -3,10 +3,10 @@ const initGrid = () => {
   if (canvas.getContext) {
     let ctx = canvas.getContext('2d');
     // Grabs data stored in the canvas tag
-    console.log(canvas.dataset.pixelArray);
-    let array = Uint8ClampedArray.from(canvas.dataset.pixelArray);
-    console.log(array);
-    gridImage = new ImageData(array, 1000, 1000);
+    let pixelArray = JSON.parse(canvas.dataset.pixelArray);
+    pixelArray = Uint8ClampedArray.from(pixelArray);
+    console.log(pixelArray);
+    let gridImage = new ImageData(pixelArray, 1000, 1000);
     ctx.putImageData(gridImage, 0, 0);
   } else {
     // Code to run when canvas is unsupported
