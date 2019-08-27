8a721ab1113aec1424ffcd783e358856fb923e89
Max Synnott <maxryansynnott@gmail.com>
2019-08-28 00:41:31 +0200

    cleaning

--- app/javascript/modules/init_grid.js
+++ app/javascript/modules/init_grid.js
@@ -1,15 +1,13 @@
 const initGrid = () => {
-  var canvas = document.getElementById('grid');
+  let canvas = document.getElementById('grid');
   if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+    let ctx = canvas.getContext('2d');
     // Grabs data stored in the canvas tag
-    const placements = JSON.parse(canvas.dataset.placements);
-    const pixel_size = canvas.dataset.pixelSize
-    // Iterates over the placements, drawing each one to the grid
-    placements.forEach((placement) => {
-      ctx.fillStyle = placement['color'];
-      ctx.fillRect(placement['x'], placement['y'], pixel_size, pixel_size);
-    })
+    console.log(canvas.dataset.pixelArray);
+    let array = Uint8ClampedArray.from(canvas.dataset.pixelArray);
+    console.log(array);
+    gridImage = new ImageData(array, 1000, 1000);
+    ctx.putImageData(gridImage, 0, 0);
   } else {
     // Code to run when canvas is unsupported
   };
