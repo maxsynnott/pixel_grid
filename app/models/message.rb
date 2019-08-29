fa307c1153b93b800df731951fde5d95a133cafb
Enrique <hhfolte@gmail.com>
2019-08-29 18:46:51 +0200

    add assosiations for message with relations to chat_room, user and also adding validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,16 @@
 class Message < ApplicationRecord
+  after_create :broadcast_message
+  belongs_to :chat_room
+  belongs_to :user
+  validates :content, presence: true, allow_blank: false
+
+  def broadcast_message
+    ActionCable.server.broadcast("chat_room_#{chat_room.id}", {
+      message_partial: ApplicationController.renderer.render(
+        partial: "messages/message",
+        locals: { message: self, user_is_messages_author: false }
+      ),
+      current_user_id: user.id
+    })
+  end
 end
