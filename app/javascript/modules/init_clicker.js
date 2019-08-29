b96182c54891ca51bf42a8f91e2d65c23638f324
Dashiell Bell <dbell@btr.pm>
2019-08-29 18:19:40 +0200

    added some comments and removed an empty line

--- app/javascript/modules/init_clicker.js
+++ app/javascript/modules/init_clicker.js
@@ -1,8 +1,10 @@
 function initClicker() {
+  // Get color grid, canvas, store as variables and add context for canvas writes.
   let cols = document.querySelector(".cols-container");
   let canvas = document.getElementById('grid');
   let ctx = canvas.getContext('2d');
 
+  // getting the mouse coordinates.
   function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -11,25 +13,10 @@ function initClicker() {
     };
   }
 
-  const rgbas = { 0: [255, 255, 255, 1],
-                 1: [87, 87, 87, 1],
-                 2: [173, 35, 35, 1],
-                 3: [42, 75, 215, 1],
-                 4: [29, 105, 20, 1],
-                 5: [129, 74, 25, 1],
-                 6: [129, 38, 192, 1],
-                 7: [160, 160, 160, 1],
-                 8: [129, 197, 122, 1],
-                 9: [157, 175, 255, 1],
-                 10: [41, 208, 208, 1],
-                 11: [255, 146, 51, 1],
-                 12: [255, 238, 51, 1],
-                 13: [233, 222, 187, 1],
-                 14: [255, 205, 243, 1],
-                 15: [0, 0, 0, 1] }
-
   let colour = "black";
 
+  // Loop over all child divs of cols-container and add event listener to them that
+  // sets the value of colour to the background color of the selected palette tile.
   for (let i = 0; i < cols.children.length; i++) {
     cols.children[i].addEventListener('click', (event) => { colour = getComputedStyle(event.currentTarget).backgroundColor })
     }
