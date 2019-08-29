229f814e4a1477ca229d1033ad654936f711e6d0
Enrique <hhfolte@gmail.com>
2019-08-29 19:19:14 +0200

    add routes via resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   
-  get 'chat_rooms/:id(.:format)', to: 'chat_rooms#show'
-  post 'chat_rooms/:chat_room_id/messages(.:format)', to: 'messages#create' as: chat_room_messages
 
+  #post 'chat_rooms/:chat_room_id/messages(.:format)', to: 'messages#create', as: chat_room_messages
+  #get 'chat_rooms/:id(.:format)', to: 'chat_rooms#show', as: chat_room
+  
+  resources :messages, only: [:create]
+  resources :chat_rooms, only: [:show]
   resources :grids, only: [:show]
 end
