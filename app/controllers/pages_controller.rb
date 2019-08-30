86eea9b280b5d780644d34a7e5adece695e9f167
Dashiell Bell <dbell@btr.pm>
2019-08-30 11:35:01 +0200

    render home page as show page for the latest grid.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  # skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    redirect_to grid_path(Grid.last)
+    # redirect_to grid_path(Grid.last)
+    @grid = Grid.last
+    render 'grids/show'
   end
 end
