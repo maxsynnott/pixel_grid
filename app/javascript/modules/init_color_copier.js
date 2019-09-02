5499c7ac85a7e5332548f5651bc8dd70b5b20372
Max Synnott <maxryansynnott@gmail.com>
2019-09-02 15:21:48 +0200

    fixed bug with color copier

--- app/javascript/modules/init_color_copier.js
+++ app/javascript/modules/init_color_copier.js
@@ -2,7 +2,7 @@ const initColorCopier = () => {
   document.addEventListener('keyup', (event) => {
     if (event.key === "Control") {
       const rgba = ctx.getImageData(mouseX, mouseY, 1, 1).data;
-      color = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${rgba[3] / 255})`;
+      color = `rgb(${rgba[0]}, ${rgba[1]}, ${rgba[2]}`;
     }
   });
 };
