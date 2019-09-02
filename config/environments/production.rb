cc659609b66fae2a916978dfaae5e117bb52c37c
Max Synnott <maxryansynnott@gmail.com>
2019-09-02 15:46:47 +0200

    config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://pixel-grid.herokuapp.com"
+  config.action_cable.allowed_request_origins = ['https://www.thepixelgrid.org', 'http://www.thepixelgrid.org', 'https://pixel-grid.herokuapp.com', 'http://pixel-grid.herokuapp.com']
 end
