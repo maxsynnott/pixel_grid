8221eafa921ec0810ee31c3d5850170bf660de1b
Max Synnott <maxryansynnott@gmail.com>
2019-09-03 22:49:45 +0200

    slight refactoring

--- app/javascript/modules/init_tracker.js
+++ app/javascript/modules/init_tracker.js
@@ -1,21 +1,19 @@
 function initTracker() {
   const hoverCheck = async (x, y) => {
-    const startX = x
-    const startY = y
-    setTimeout(() => { if ((startX == mouseX) && (startY == mouseY)) {
-                         const imgData = ctx.getImageData(startX, startY, 1, 1);
+    setTimeout(() => { if ((x == mouseX) && (y == mouseY)) {
+                         const imgData = ctx.getImageData(x, y, 1, 1);
                          if (JSON.stringify([imgData, x, y]) != JSON.stringify(currentHover)) {
                            const alpha = imgData.data[3];
-                           imgData.data[3] = 150;
-                           ctx.putImageData(imgData, startX, startY);
+                           imgData.data[3] = 150; // Wanted hover alpha value, 0-255
+                           ctx.putImageData(imgData, x, y);
                            if (currentHover.length != 0) {
                              currentHover[0].data[3] = alpha
                              ctx.putImageData(currentHover[0], currentHover[1], currentHover[2])
                            }
-                           currentHover = [imgData, startX, startY]
+                           currentHover = [imgData, x, y]
                          };
                        }
-                     }, 50);
+                     }, 50); // Number of miliseconds before pixel is highlighted
   }
 
   canvas.addEventListener('mousemove', (event) => {
