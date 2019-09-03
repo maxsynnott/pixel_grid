83532b780fba63eda34945a6e58aa6fb146d1dc3
Dashiell Bell <dbell@btr.pm>
2019-09-03 19:51:35 +0200

    Added some nice UI tings

--- app/javascript/modules/init_color_copier.js
+++ app/javascript/modules/init_color_copier.js
@@ -1,8 +1,19 @@
 const initColorCopier = () => {
+  const regex = new RegExp('[r|g|b|(|)| ]', 'g')
   document.addEventListener('keyup', (event) => {
     if (event.key === "Control") {
       const rgba = ctx.getImageData(mouseX, mouseY, 1, 1).data;
       color = `rgb(${rgba[0]}, ${rgba[1]}, ${rgba[2]}`;
+      let rgb = (color.replace(regex, '').split(','));
+      rgb.push(255)
+      const rgbaArray = rgb.map( num => parseInt(num, 10));
+      const colorIndex = parseInt(Object.keys(rgbas).find(key => JSON.stringify(rgbas[key]) === JSON.stringify(rgbaArray)), 10);
+      const cols = document.querySelector(".cols-container");
+      const current = document.querySelector(".active")
+      if (current) {
+        current.classList.remove('active')
+      }
+      cols.children[colorIndex].classList.add('active')
     }
   });
 };
