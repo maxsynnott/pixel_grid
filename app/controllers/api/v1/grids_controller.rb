38b439e77f36f198dc1734aacc84af18e65b1d5c
Max Synnott <maxryansynnott@gmail.com>
2019-09-04 21:38:32 +0200

    validation at api level

--- app/controllers/api/v1/grids_controller.rb
+++ app/controllers/api/v1/grids_controller.rb
@@ -1,8 +1,20 @@
 class Api::V1::GridsController < Api::V1::BaseController
   def place_pixel
-    PixelUpdateJob.perform_later(grid_id: params[:id],
-                                 x: params[:x],
-                                 y: params[:y],
-                                 color_index: params[:colorIndex])
+    validated = true
+    @grid = false
+
+    @grid = Grid.find(params[:id]) if (0..Grid.last.id).include?(params[:id])
+    if @grid
+      validated = false if (0..@grid.width).include?(params[:x])
+      validated = false if (0..@grid.height).include?(params[:y])
+      validated = false if (0..15).include?(params[:colorIndex])
+    end
+
+    if validated
+      PixelUpdateJob.perform_later(grid_id: params[:id],
+                                   x: params[:x],
+                                   y: params[:y],
+                                   color_index: params[:colorIndex])
+    end
   end
 end
