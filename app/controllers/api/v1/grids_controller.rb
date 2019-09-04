6b757daf0c415b2fb3099f38e1ca4d741805212e
Max Synnott <maxryansynnott@gmail.com>
2019-09-04 22:13:11 +0200

    fix

--- app/controllers/api/v1/grids_controller.rb
+++ app/controllers/api/v1/grids_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::GridsController < Api::V1::BaseController
     @validated = true
     @grid = false
 
-    @grid = Grid.find(params[:id]) if (0..Grid.last.id).include?(params[:id])
+    @grid = Grid.find(params[:id]) if (0..Grid.last.id).include?(params[:id].to_i)
     if @grid
-      @validated = false if (0...@grid.width).include?(params[:x])
-      @validated = false if (0...@grid.height).include?(params[:y])
-      @validated = false if (0..15).include?(params[:colorIndex])
+      @validated = false if (0...@grid.width).include?(params[:x].to_i)
+      @validated = false if (0...@grid.height).include?(params[:y].to_i)
+      @validated = false if (0..15).include?(params[:colorIndex].to_i)
     else
       @validated = false
     end
