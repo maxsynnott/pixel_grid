b617b5d2a48a632264eb353847b71ede95edb61c
Max Synnott <maxryansynnott@gmail.com>
2019-09-05 15:34:23 +0200

    removed effect listeners

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@ import { initTracker } from '../modules/init_tracker';
 import { initColsPicker } from '../modules/init_colspicker';
 import { initClicker } from '../modules/init_clicker';
 import { initColorCopier } from '../modules/init_color_copier';
-import { initEffectListeners } from '../modules/init_effect_listeners';
 import { initImgPainter } from '../modules/init_img_painter';
 
 initGrid();
@@ -15,5 +14,4 @@ initTracker();
 initColsPicker();
 initClicker();
 initColorCopier();
-initEffectListeners();
 initImgPainter();
