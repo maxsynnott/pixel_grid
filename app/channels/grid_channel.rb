feed4f83b4c0550530944c30b385380063a024ad
Max Synnott <maxryansynnott@gmail.com>
2019-09-09 21:22:14 +0200

    added online user count feature

--- app/channels/grid_channel.rb
+++ app/channels/grid_channel.rb
@@ -2,9 +2,11 @@ class GridChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     stream_from "grid_#{params[:grid_id]}"
+    ActionCable.server.broadcast("grid_#{params[:grid_id]}", users: ActionCable.server.connections.length)
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    ActionCable.server.broadcast("grid_#{params[:grid_id]}", users: ActionCable.server.connections.length)
   end
 end
