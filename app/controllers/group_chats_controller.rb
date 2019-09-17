24f7265381186b216f2a01dc93364668d73e76c9
Max Synnott <maxryansynnott@gmail.com>
2019-09-17 14:27:23 +0200

    added new group chat functionality

--- app/controllers/group_chats_controller.rb
+++ app/controllers/group_chats_controller.rb
@@ -1,8 +1,19 @@
 class GroupChatsController < ApplicationController
   def create
+    @group_chat = GroupChat.new(group_chat_params)
+    @group_chat.grid = Grid.find(params[:grid_id])
+    @group_chat.save
+
+    redirect_to grid_path(@group_chat.grid)
   end
 
   def show
     @group_chat = GroupChat.find(params[:id])
+  end
+
+  private
+
+  def group_chat_params
+    params.require(:group_chat).permit(:name)
   end
 end
