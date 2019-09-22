2dbf1df731cf6217673c3d96662213679391a97a
Dashiell Bell <dbell@btr.pm>
2019-09-22 14:32:32 +0200

    frontend chat prototype

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,18 @@ ActiveRecord::Schema.define(version: 2019_09_15_150436) do
     t.integer "user_id"
   end
 
+  create_table "placements", force: :cascade do |t|
+    t.string "color"
+    t.bigint "user_id"
+    t.bigint "grid_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "x"
+    t.integer "y"
+    t.index ["grid_id"], name: "index_placements_on_grid_id"
+    t.index ["user_id"], name: "index_placements_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -70,4 +82,6 @@ ActiveRecord::Schema.define(version: 2019_09_15_150436) do
   add_foreign_key "group_chats", "grids"
   add_foreign_key "messages", "group_chats"
   add_foreign_key "messages", "users"
+  add_foreign_key "placements", "grids"
+  add_foreign_key "placements", "users"
 end
